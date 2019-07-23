192b1267fad952381247c6a5c4bee211e68dbe02
Hamanhcuong26 <53035856+Hamanhcuong26@users.noreply.github.com>
2019-07-23 09:04:13 +0800

    Update broken_score.py

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -11,9 +11,9 @@ if score < 0:
 else:
     if score > 100:
         print("Invalid score")
-    if score > 50:
+    if score > 50 and score < 90:
         print("Passable")
-    if score > 90:
-    print("Excellent")
+    if score > 90 and score < 100:
+        print("Excellent")
 if score < 50:
     print("Bad")
